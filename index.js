2025788cd84b04aa7e35103b7e6ad961c8f5390c
Robin La Rondelle <ra.larondelle@student.avans.nl>
2020-04-23 13:37:13 +0200

    first connection to twitter API

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 require("dotenv").config({ path: "./environment/development.env" })
-console.log(process.env.ACCESS_TOKEN)
+
+console.log(process.env.API_KEY);
+console.log(process.env.API_KEY_SECRET);
+console.log(process.env.ACCESS_TOKEN);
+console.log(process.env.ACCESS_TOKEN_SECRET);
+
+
+const twitter = require('twitter')
+const client = new twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+const params ={screen_name: 'feyenoord'}
+
+client.get('statuses/user_timeline', params, (err, tweets, response) => {
+  if (!err) {
+    console.log(tweets);
+    
+  } else {
+    console.log(err);
+    
+  }
+})
