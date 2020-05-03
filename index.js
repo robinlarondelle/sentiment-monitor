fcac24af7b8e76d0c8b1255a4cfbca87fb46b216
Robin La Rondelle <ra.larondelle@student.avans.nl>
2020-05-03 21:25:19 +0200

    update

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 require("dotenv").config({ path: "./environment/development.env" })
 const twitter = require('twitter')
+const moment = require("moment")
 const { filter } = require('rxjs')
-const fs  = require('fs')
-
+const natural = require("natural")
+const tokenizer = new natural.WordTokenizer();
 const client = new twitter({
   consumer_key: process.env.API_KEY,
   consumer_secret: process.env.API_SECRET_KEY,
   access_token_key: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
-
+const until = moment().subtract(6, "days").format("yyyy-MM-DD")
 const params ={
   q: 'coca cola', 
   lang: 'en', 
   include_entities: false,
   count: 100,
-  until: '2020-04-18'
+  until
 }
 
 client.get('search/tweets', params)
   .then(tweets => {
-    const filteredTweetsText = filterTweets(tweets.statuses).map(x => x.text)
-    writeFile(filteredTweetsText, 'filtered_tweets')
-  }).catch(error => console.log(error))
+    filterTweets(tweets.statuses.map(x => x.text)).then(filteredTweets => {
+      tokenizeTweets(filteredTweets).then(tokenizedTweets => {
+        console.log(tokenizedTweets);
+      }).catch(error => console.log(error))
+    }).catch(error => console.log(error))
+  }).catch(err => console.log(err))
 
-function filterTweets(tweetsList) 
-  return tweetsList.filter(x => !(/^RT @.*/.test(x.text)))
 
-function checkForFile(filename, callback) {
-  fs.exists(filename, exists => {
-    if (exists) callback()
-    else {
-      fs.writeFile(filename, {flag: 'wx'}, (err, data) => { 
-          callback();
-      })
-    }
+function filterTweets(tweetsList) {
+  return new Promise((resolve, reject) => {
+    resolve(tweetsList.filter(x => !(/^RT @.*/.test(x))))
   })
 }
 
-function writeFile(json, path) {
-  const filename = `./file/${path}.json`
 
-  checkForFile(filename, () => {
-    fs.writeFileSync(filename, JSON.stringify(json, null, 2),'utf-8', { flag: 'wx' }, err => {
-      if (err) console.log(err); return;
-    })
+function tokenizeTweets(tweets) {
+  return new Promise((resolve, reject) => {
+    resolve(tweets.map(x => tokenizer.tokenize(x)))
   })
 }
